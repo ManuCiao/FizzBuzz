8e4b9cb16de9763ebc6e13a4e67c56375e9978f9
ManuCiao <mn.sabatino@gmail.com>
2016-09-20 01:48:00 +0200

    Add FizzBuzz first tempt of rspec

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,29 @@
+require '../lib/fizzbuzz'
+
+describe "fizzbuzz" do
+  it "should return 'Fizz' for number divisible by 3" do
+    expect(fizzbuzz(3)).to eq "Fizz"
+    expect(fizzbuzz(6)).to eq "Fizz"
+    expect(fizzbuzz(9)).to eq "Fizz"
+  end
+
+  it "should return 'Buzz' for number divisible by 5" do
+    expect(fizzbuzz(5)).to eq "Buzz"
+    expect(fizzbuzz(10)).to eq "Buzz"
+  end
+
+  it "should return 'fizzbuzz' for number divisible by 3 and 5" do
+    expect(fizzbuzz(15)).to eq "FizzBuzz"
+    expect(fizzbuzz(30)).to eq "FizzBuzz"
+  end
+
+  it "should return `fizzbuzz` for number divisible by 3 and 5" do
+    expect(fizzbuzz(90)).to eq "FizzBuzz"
+  end
+
+  it "should return the integer if not divisible by 3 or 5" do
+    expect(fizzbuzz(1)).to eq 1
+    expect(fizzbuzz(7)).to eq 7 
+ end
+end
+
