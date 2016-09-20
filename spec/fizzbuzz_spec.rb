9a96ecd6796fbb1c00b17d7351c3744431c1060a
ManuCiao <mn.sabatino@gmail.com>
2016-09-20 22:28:46 +0200

    Another way to write the rspec

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,25 +1,19 @@
-require '../lib/fizzbuzz'
+require 'fizzbuzz'
 
-describe "fizzbuzz" do
-  it "should return 'Fizz' for number divisible by 3" do
-    expect(fizzbuzz(3)).to eq "Fizz"
-    expect(fizzbuzz(6)).to eq "Fizz"
-    expect(fizzbuzz(9)).to eq "Fizz"
+describe 'FizzBuzz' do
+  
+  context 'while playing the game it returns' do
+    it 'the number' do
+      expect(fizzbuzz(1)).to eq 1
+    end
+    it 'Fizz' do
+      expect(fizzbuzz(3)).to eq "Fizz"
+    end
+    it 'Buzz' do
+      expect(fizzbuzz(5)).to eq "Buzz"
+    end
+    it "FizzBuzz" do
+      expect(fizzbuzz(15)).to eq "FizzBuzz"
+    end
   end
-
-  it "should return 'Buzz' for number divisible by 5" do
-    expect(fizzbuzz(5)).to eq "Buzz"
-    expect(fizzbuzz(10)).to eq "Buzz"
-  end
-
-  it "should return 'fizzbuzz' for number divisible by 3 and 5" do
-    expect(fizzbuzz(15)).to eq "FizzBuzz"
-    expect(fizzbuzz(30)).to eq "FizzBuzz"
-  end
-
-  it "should return the integer if not divisible by 3 or 5" do
-    expect(fizzbuzz(1)).to eq 1
-    expect(fizzbuzz(7)).to eq 7 
- end
 end
-
